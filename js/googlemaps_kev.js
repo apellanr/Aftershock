ca5d8e62d13245be8ec913c2501530bd4808ada9
fastexposure <fastexposure@yahoo.com>
2017-03-22 12:34:13 -0700

    clean up josh.js

--- js/googlemaps_kev.js
+++ js/googlemaps_kev.js
@@ -1,37 +1,66 @@
-
 var map;
+var infowindow;
+var request;
+var service;
+var markers = [];
 
 $(document).ready(mapInit);
 
 function mapInit() {
-    var center = new google.maps.LatLng(37.422, -122.084058);
-    map = new google.maps.Map(document.getElementById('map'), {
-       center: center,
-        zoom: 13
-    });
-    var request = {
-        location: center,
-        radius: 8047,
-        types: ['cafe'] // Change this
-    };
+   var center = new google.maps.LatLng(37.422, -122.084058);
+   map = new google.maps.Map(document.getElementById('map'), {
+      center: center,
+       zoom: 13
+   });
+   request = {
+       location: center,
+       radius: 8047,
+       types: ['cafe'] // Change this
+   };
 
-    var service = new google.maps.places.PlacesService(map);
-    service.nearbySearch(request, callback);
+   infowindow = new google.maps.InfoWindow();
 
+   service = new google.maps.places.PlacesService(map);
+   service.nearbySearch(request, callback);
+
+   google.maps.event.addListener(map, 'rightclick', function (event) {
+       map.setCenter(event.latLng);
+       clearResults(markers);
+
+       var request = {
+           location: event.latLng,
+           radius: 8047,
+           types: ['cafe']
+       };
+       service.nearbySearch(request, callback);
+   })
 }
 
 function callback(results, status) {
-    if (status == google.maps.places.PlacesServiceStatus.OK) {
-        for (var i = 0 ; i < results.length ; i++) {
-            createMarker(results[i]);
-        }
-    }
+   if (status == google.maps.places.PlacesServiceStatus.OK) {
+       for (var i = 0 ; i < results.length ; i++) {
+           markers.push(createMarker(results[i]));
+       }
+   }
 }
 
 function createMarker(place) {
-    var placeLoc = place.geometry.location;
-    var marker = new google.maps.Marker({
-        map: map,
-        position: place.geometry.location
-    });
+   var placeLoc = place.geometry.location;
+   var marker = new google.maps.Marker({
+       map: map,
+       position: place.geometry.location
+   });
+
+   google.maps.event.addListener(marker, 'click', function () {
+       infowindow.setContent(place.name);
+       infowindow.open(map, this);
+   });
+   return marker;
+}
+
+function clearResults(markers) {
+   for (var m in markers) {
+       markers[m].setMap(null)
+   }
+   markers = [];
 }
