27805e89a4dc1cb3ec58ee49ba021a63a1f3322e
apellanr <ryan.apellanes@gmail.com>
2017-03-22 12:16:00 -0700

    major changes members files - add ons

--- js/googlemaps_kev.js
+++ js/googlemaps_kev.js
@@ -1,29 +1,45 @@
-
 var map;
+var infowindow;
+var request;
+var service;
+var markers = [];
 
 $(document).ready(mapInit);
 
 function mapInit() {
     var center = new google.maps.LatLng(37.422, -122.084058);
     map = new google.maps.Map(document.getElementById('map'), {
-       center: center,
+        center: center,
         zoom: 13
     });
-    var request = {
+    request = {
         location: center,
         radius: 8047,
         types: ['cafe'] // Change this
     };
 
-    var service = new google.maps.places.PlacesService(map);
+    infowindow = new google.maps.InfoWindow();
+
+    service = new google.maps.places.PlacesService(map);
     service.nearbySearch(request, callback);
 
+    google.maps.event.addListener(map, 'rightclick', function (event) {
+        map.setCenter(event.latLng);
+        clearResults(markers);
+
+        var request = {
+            location: event.latLng,
+            radius: 8047,
+            types: ['cafe']
+        };
+        service.nearbySearch(request, callback);
+    })
 }
 
 function callback(results, status) {
     if (status == google.maps.places.PlacesServiceStatus.OK) {
         for (var i = 0 ; i < results.length ; i++) {
-            createMarker(results[i]);
+            markers.push(createMarker(results[i]));
         }
     }
 }
@@ -34,4 +50,17 @@ function createMarker(place) {
         map: map,
         position: place.geometry.location
     });
+
+    google.maps.event.addListener(marker, 'click', function () {
+        infowindow.setContent(place.name);
+        infowindow.open(map, this);
+    });
+    return marker;
+}
+
+function clearResults(markers) {
+    for (var m in markers) {
+        markers[m].setMap(null)
+    }
+    markers = [];
 }
