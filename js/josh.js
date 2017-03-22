b3c04bf6035cbb4c756b2702a5ca47f00b33b800
Zinu <Zinu>
2017-03-22 15:33:00 -0700

    added ajax calls for day and week

--- js/josh.js
+++ js/josh.js
@@ -1,29 +1,89 @@
-// josh start work ong josh.js
+// jinwoo start work ong josh.js
 $().ready(startUSGS);
 //global var
 var usgsData = null;
-var eqArray30DaysM4p5 = [];
+var eqArrayWeekM4p5 = [];
+var eqArrayMonthM4p5 = [];
+var eqArrayDayM4p5 = [];
+
 //global var
 function startUSGS(){
 	usgsData = new ConstructorUSGS;
-	usgsData.getUSGS();
+    usgsData.getUSGSWeek();
+    usgsData.getUSGSMonth();
+    usgsData.getUSGSDay();
 }
 function ConstructorUSGS(){
 	var self = this;
-	this.getUSGS = function(){
+
+	this.getUSGSWeek= function(){
 		$.ajax({
-			url: 'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_month.geojson',
+			url: 'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_week.geojson',
 			method: 'get',
 			success: function(returnResponse){
-				self.sortUSGSData(returnResponse);
-			},
+				console.log(returnResponse);
+				self.sortUSGSWeek(returnResponse);
+            },
 			error: function(returnResponse){
-				thisObj.displayServerModal('Delete Error: ' + returnResponse.responseText, "Status Code: " + returnResponse.status);
-				console.log('error ', returnResponse);
-			}
-		})
-	}
-	this.sortUSGSData = function(returnResponse){
+                self.displayServerModal('Delete Error: ' + returnResponse.responseText, "Status Code: " + returnResponse.status);
+                console.log('error ', returnResponse);
+            }
+		});
+	};
+    this.getUSGSMonth = function(){
+    	$.ajax({
+    		url: 'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_month.geojson',
+    		method: 'get',
+    		success: function(returnResponse){
+                console.log(returnResponse);
+    			self.sortUSGSMonth(returnResponse);
+    		},
+    		error: function(returnResponse){
+    			self.displayServerModal('Delete Error: ' + returnResponse.responseText, "Status Code: " + returnResponse.status);
+    			console.log('error ', returnResponse);
+    		}
+    	})
+    };
+    this.getUSGSDay = function(){
+        $.ajax({
+            url: 'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_day.geojson',
+            method: 'get',
+            success: function(returnResponse){
+                console.log(returnResponse);
+                self.sortUSGSDay(returnResponse);
+            },
+            error: function(returnResponse){
+                self.displayServerModal('Delete Error: ' + returnResponse.responseText, "Status Code: " + returnResponse.status);
+                console.log('error ', returnResponse);
+            }
+        })
+    };
+	this.sortUSGSWeek = function(returnResponse){
+		for (var i = 0; i < returnResponse.features.length; i++) {
+			var location = returnResponse.features[i].properties.place;
+			var mag = returnResponse.features[i].properties.mag;
+			var utcSecond = returnResponse.features[i].properties.time;
+			var time = new Date(utcSecond);
+			var long = returnResponse.features[i].geometry.coordinates[0];
+			var lat = returnResponse.features[i].geometry.coordinates[1];
+			var depth = returnResponse.features[i].geometry.coordinates[2];
+            eqArrayWeekM4p5.push({location, mag, time, lat, long, depth});
+		}
+	};
+    this.sortUSGSMonth = function(returnResponse){
+        for (var i = 0; i < returnResponse.features.length; i++) {
+            var location = returnResponse.features[i].properties.place;
+            var mag = returnResponse.features[i].properties.mag;
+            var utcSecond = returnResponse.features[i].properties.time;
+            var time = new Date(utcSecond);
+            var long = returnResponse.features[i].geometry.coordinates[0];
+            var lat = returnResponse.features[i].geometry.coordinates[1];
+            var depth = returnResponse.features[i].geometry.coordinates[2];
+            eqArrayMonthM4p5.push({location, mag, time, lat, long, depth});
+
+        }
+    };
+	this.sortUSGSDay = function(returnResponse){
 		for(var i = 0; i < 	returnResponse.features.length; i++){
 			var location = returnResponse.features[i].properties.place;
 			var mag = returnResponse.features[i].properties.mag;
@@ -32,9 +92,8 @@ function ConstructorUSGS(){
 			var lat = returnResponse.features[i].geometry.coordinates[1];
 			var long = returnResponse.features[i].geometry.coordinates[0];
 			var depth = returnResponse.features[i].geometry.coordinates[2];
-			eqArray30DaysM4p5.push({location, mag, time, lat, long, depth})
+            eqArrayDayM4p5.push({location, mag, time, lat, long, depth});
 		}
-
 	}
 }
-// josh end work ong josh.js
+// Jinwoo end work ong josh.js
